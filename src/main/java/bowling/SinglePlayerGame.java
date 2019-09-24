312098874bc1f2280b6922702628593d15b1ddf7
pedago <pedago@pcalb-mm0616.univ-jfc.fr>
2019-09-24 17:18:37 +0200

    TP3

--- src/main/java/bowling/SinglePlayerGame.java
+++ src/main/java/bowling/SinglePlayerGame.java
@@ -7,10 +7,20 @@ package bowling;
  */
 public class SinglePlayerGame {
 
+        private int scorefinal;
+        private int quilleRestantes;
+        private int strike;
+        private int spare;
+        private int compteur;
 	/**
 	 * Constructeur
 	 */
 	public SinglePlayerGame() {
+            this.scorefinal = 0;
+            this.quilleRestantes = 10;
+            this.strike = 0;
+            this.spare = 0;
+            this.compteur = 0;
 	}
 
 	/**
@@ -20,7 +30,51 @@ public class SinglePlayerGame {
 	 * ce lancé
 	 */
 	public void lancer(int nombreDeQuillesAbattues) {
+          if(this.compteur<22){
+                // point du spare d'avant
+                if(this.spare == 1){
+                     this.scorefinal += nombreDeQuillesAbattues;
+                     this.spare = 0;
+                }
+                // point du strike d'avant
+                if(this.strike > 0){
+                     this.scorefinal += nombreDeQuillesAbattues;
+                     this.strike--;
+                }
 
+                // cas normal
+                if(this.quilleRestantes > nombreDeQuillesAbattues){
+                    this.scorefinal += nombreDeQuillesAbattues;
+                    this.quilleRestantes -= nombreDeQuillesAbattues;
+                    this.compteur++;
+                }
+
+                //spare
+                // si on est au deuxieme lancé et qu'on fait tomber toutes les quilles restantes
+                if(((this.compteur%2) == 1)&&(nombreDeQuillesAbattues == this.quilleRestantes) && (this.compteur<20)){
+                    this.spare = 1;
+                    this.scorefinal += nombreDeQuillesAbattues;
+                    this.quilleRestantes -= nombreDeQuillesAbattues;
+                    this.compteur++;
+                }
+
+                //strike
+                if (((this.compteur%2) == 0) && (nombreDeQuillesAbattues == 10) && (this.compteur<20)){
+                    this.strike = 2;
+                    this.scorefinal += nombreDeQuillesAbattues;
+                    this.quilleRestantes -= nombreDeQuillesAbattues;
+                    this.compteur+=2;
+                }
+
+
+                if(this.quilleRestantes == 0 || (this.compteur%2) == 0){
+                    this.quilleRestantes = 10;
+                }
+          }
+          else{
+              System.out.println("La partie est fini!");
+          }
+                 
 	}
 
 	/**
@@ -29,6 +83,9 @@ public class SinglePlayerGame {
 	 * @return Le score du joueur
 	 */
 	public int score() {
-		throw new UnsupportedOperationException("Pas encore implémenté");
+		//throw new UnsupportedOperationException("Pas encore implémenté");
+                
+            return this.scorefinal;   
+                
 	}
 }
